593de7c5061443c81f4990edffb95b66178f45d5
Ayesh Karunaratne <ayesh@ayesh.me>
2020-02-09 00:16:37 +0200

    more

--- tests/phpunit/includes/mock-mailer.php
+++ tests/phpunit/includes/mock-mailer.php
@@ -18,7 +18,7 @@ class MockPHPMailer extends PHPMailer\PHPMailer\PHPMailer {
 			'to'      => $this->to,
 			'cc'      => $this->cc,
 			'bcc'     => $this->bcc,
-			'header'  => iconv_mime_decode_headers( $this->MIMEHeader . $this->mailHeader ),
+			'header'  => iconv_mime_decode( $this->MIMEHeader . $this->mailHeader ),
 			'subject' => $this->Subject,
 			'body'    => $this->MIMEBody,
 		);
