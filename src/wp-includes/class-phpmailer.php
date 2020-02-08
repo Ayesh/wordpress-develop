206a4f72d219286005721b7984d411d256770b84
Ayesh Karunaratne <ayesh@ayesh.me>
2020-02-08 23:15:08 +0200

    Add _deprecated_file() calls to smtp and phpmailer files

--- src/wp-includes/class-phpmailer.php
+++ src/wp-includes/class-phpmailer.php
@@ -3,4 +3,5 @@
 /**
  * PHPMailer class is now moved to the PHPMailer sub directory with PHPMailer 6+.
  */
+_deprecated_file( basename( __FILE__ ), '5.4.0', WPINC . '/PHPMailer/PHPMailer.php', __( 'PHPMailer 6+ versions are namespaced and are relocated to PHPMailer subdirectory.' ) );
 include __DIR__ .'/PHPMailer/PHPMailer.php';
